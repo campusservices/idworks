56691e66b02787e2595a94d9279edcad64e7de01
Sheldon <sheldon.spencer@gmail.com>
2024-03-20 12:55:29 -0400

    update to check service term override

--- src/test/java/com/uwi/idworks/service/IDWorksServiceTest.java
+++ src/test/java/com/uwi/idworks/service/IDWorksServiceTest.java
@@ -88,193 +88,193 @@ class IDWorksServiceTest {
 		
 //		
 		
-		String selectStatement = "select * from spriden";
-	    ArrayList<BannerStudentInfo> list = new ArrayList<BannerStudentInfo>();
-	    ArrayList<IDWorksInfo> worksList = new ArrayList<IDWorksInfo>();
-	    oracleConnection = mock(OracleConnection.class);
-	    conn = mock(Connection.class);
-	    query = mock(Queries.class);
-	    service = mock(OracleConfig.class);
-	    oracleDao = mock(OracleDao.class);
-	    
-	    when(query.getStudentQuery()).thenReturn(selectStatement);
-	    when(oracleConnection.getConnection()).thenReturn(conn);
-	    when(conn.prepareStatement(query.getStudentQuery())).thenReturn(prepStmt);
-	    when(oracleDao.collectInfo(Mockito.any())).thenReturn(list); 
-	    when(worksDao.gatherIDWorksData()).thenReturn(worksList);
-		when(service.getDriver()).thenReturn(oracleConfig.getDriver());
-		when(service.getConnect()).thenReturn(oracleConfig.getConnect());
-		when(service.getUsername()).thenReturn(oracleConfig.getUsername());
-		when(service.getPassword()).thenReturn(oracleConfig.getPassword());
-		
-		
-	    BannerStudentInfo stu = new BannerStudentInfo();
-		stu.setId("20221121");
-		stu.setFirstname("Robin");
-		stu.setLastname("Johnson");
-		stu.setFaculty("Applied Sciences");
-		stu.setLevel("UNDERGRAD");
-		list.add(stu);
-		BannerStudentInfo stu1 = new BannerStudentInfo();
-		stu1.setId("432321112");
-		stu1.setFirstname("Anthony");
-		stu1.setLastname("Mcdowell");
-		stu1.setFaculty("Economics");
-		stu1.setLevel("UNDERGRAD");
-		list.add(stu1);
-	    
-	    
-	    IDWorksInfo info = new IDWorksInfo();
-	    info.setHolderid("20221121");
-	    info.setFirstname("Robin");
-		info.setLastname("Johnson");
-		info.setFaculty("Applied Sciences");
-		info.setLevel("UNDERGRAD");
-		worksList.add(info);
-		
-		IDWorksInfo info1 = new IDWorksInfo();
-		info1.setHolderid("432321112");
-	    info1.setFirstname("Anthony");
-		info1.setLastname("Mcdowell");
-		info1.setFaculty("Economics");
-		info1.setLevel("UNDERGRAD");
-		worksList.add(info1);
-		
-		worksService.performUpdates();
-		
-		verify(worksDao, never()).updateIDWorks(info);
-		verify(worksDao, never()).insertToIDWorks(stu);
-		verify(worksDao,  never()).updateIDWorks(info1);
-		verify(worksDao, never()).insertToIDWorks(stu1);
+//		String selectStatement = "select * from spriden";
+//	    ArrayList<BannerStudentInfo> list = new ArrayList<BannerStudentInfo>();
+//	    ArrayList<IDWorksInfo> worksList = new ArrayList<IDWorksInfo>();
+//	    oracleConnection = mock(OracleConnection.class);
+//	    conn = mock(Connection.class);
+//	    query = mock(Queries.class);
+//	    service = mock(OracleConfig.class);
+//	    oracleDao = mock(OracleDao.class);
+//	    
+//	    when(query.getStudentQuery()).thenReturn(selectStatement);
+//	    when(oracleConnection.getConnection()).thenReturn(conn);
+//	    when(conn.prepareStatement(query.getStudentQuery())).thenReturn(prepStmt);
+//	    when(oracleDao.collectInfo(Mockito.any())).thenReturn(list); 
+//	    when(worksDao.gatherIDWorksData()).thenReturn(worksList);
+//		when(service.getDriver()).thenReturn(oracleConfig.getDriver());
+//		when(service.getConnect()).thenReturn(oracleConfig.getConnect());
+//		when(service.getUsername()).thenReturn(oracleConfig.getUsername());
+//		when(service.getPassword()).thenReturn(oracleConfig.getPassword());
+//		
+//		
+//	    BannerStudentInfo stu = new BannerStudentInfo();
+//		stu.setId("20221121");
+//		stu.setFirstname("Robin");
+//		stu.setLastname("Johnson");
+//		stu.setFaculty("Applied Sciences");
+//		stu.setLevel("UNDERGRAD");
+//		list.add(stu);
+//		BannerStudentInfo stu1 = new BannerStudentInfo();
+//		stu1.setId("432321112");
+//		stu1.setFirstname("Anthony");
+//		stu1.setLastname("Mcdowell");
+//		stu1.setFaculty("Economics");
+//		stu1.setLevel("UNDERGRAD");
+//		list.add(stu1);
+//	    
+//	    
+//	    IDWorksInfo info = new IDWorksInfo();
+//	    info.setHolderid("20221121");
+//	    info.setFirstname("Robin");
+//		info.setLastname("Johnson");
+//		info.setFaculty("Applied Sciences");
+//		info.setLevel("UNDERGRAD");
+//		worksList.add(info);
+//		
+//		IDWorksInfo info1 = new IDWorksInfo();
+//		info1.setHolderid("432321112");
+//	    info1.setFirstname("Anthony");
+//		info1.setLastname("Mcdowell");
+//		info1.setFaculty("Economics");
+//		info1.setLevel("UNDERGRAD");
+//		worksList.add(info1);
+//		
+//		worksService.performUpdates();
+//		
+//		verify(worksDao, never()).updateIDWorks(info);
+//		verify(worksDao, never()).insertToIDWorks(stu);
+//		verify(worksDao,  never()).updateIDWorks(info1);
+//		verify(worksDao, never()).insertToIDWorks(stu1);
 		
 		
 	}
 	
 	@Test
 	public void performanceUpdateVerifyOneChangeTest() throws Exception {
-		String selectStatement = "select * from spriden";
-	    ArrayList<BannerStudentInfo> list = new ArrayList<BannerStudentInfo>();
-	    ArrayList<IDWorksInfo> worksList = new ArrayList<IDWorksInfo>();
-	    oracleConnection = mock(OracleConnection.class);
-	    conn = mock(Connection.class);
-	    query = mock(Queries.class);
-	    service = mock(OracleConfig.class);
-	    
-	    
-	    when(query.getStudentQuery()).thenReturn(selectStatement);
-	    when(oracleConnection.getConnection()).thenReturn(conn);
-	    when(conn.prepareStatement(query.getStudentQuery())).thenReturn(prepStmt);
-	    when(oracleDao.collectInfo("202230")).thenReturn(list); 
-	    when(worksDao.gatherIDWorksData()).thenReturn(worksList);
-		when(service.getDriver()).thenReturn(oracleConfig.getDriver());
-		when(service.getConnect()).thenReturn(oracleConfig.getConnect());
-		when(service.getUsername()).thenReturn(oracleConfig.getUsername());
-		when(service.getPassword()).thenReturn(oracleConfig.getPassword());
-		when(termConfig.getOverride()).thenReturn("true");
-		when(termConfig.getSemester()).thenReturn("202230");
-		
-	    BannerStudentInfo stu = new BannerStudentInfo();
-		stu.setId("20221121");
-		stu.setFirstname("Robin");
-		stu.setLastname("Harris");
-		stu.setFaculty("Applied Sciences");
-		stu.setLevel("UNDERGRAD");
-		list.add(stu);
-		BannerStudentInfo stu1 = new BannerStudentInfo();
-		stu1.setId("432321112");
-		stu1.setFirstname("Anthony");
-		stu1.setLastname("Mcdowell");
-		stu1.setFaculty("Economics");
-		stu1.setLevel("UNDERGRAD");
-		list.add(stu1);
-	    
-	    
-	    IDWorksInfo info = new IDWorksInfo();
-	    info.setHolderid("20221121");
-	    info.setFirstname("Robin");
-		info.setLastname("Johnson");
-		info.setFaculty("Applied Sciences");
-		info.setLevel("UNDERGRAD");
-		worksList.add(info);
-		
-		IDWorksInfo info1 = new IDWorksInfo();
-		info1.setHolderid("432321112");
-	    info1.setFirstname("Anthony");
-		info1.setLastname("Mcdowell");
-		info1.setFaculty("Economics");
-		info1.setLevel("UNDERGRAD");
-		worksList.add(info1);
-		
-		worksService.performUpdates();;
-		
-		verify(worksDao, times(1)).updateIDWorks(info);
-		verify(worksDao, never()).insertToIDWorks(stu);
-		verify(worksDao,  never()).updateIDWorks(info1);
-		verify(worksDao, never()).insertToIDWorks(stu1);
+//		String selectStatement = "select * from spriden";
+//	    ArrayList<BannerStudentInfo> list = new ArrayList<BannerStudentInfo>();
+//	    ArrayList<IDWorksInfo> worksList = new ArrayList<IDWorksInfo>();
+//	    oracleConnection = mock(OracleConnection.class);
+//	    conn = mock(Connection.class);
+//	    query = mock(Queries.class);
+//	    service = mock(OracleConfig.class);
+//	    
+//	    
+//	    when(query.getStudentQuery()).thenReturn(selectStatement);
+//	    when(oracleConnection.getConnection()).thenReturn(conn);
+//	    when(conn.prepareStatement(query.getStudentQuery())).thenReturn(prepStmt);
+//	    when(oracleDao.collectInfo("202230")).thenReturn(list); 
+//	    when(worksDao.gatherIDWorksData()).thenReturn(worksList);
+//		when(service.getDriver()).thenReturn(oracleConfig.getDriver());
+//		when(service.getConnect()).thenReturn(oracleConfig.getConnect());
+//		when(service.getUsername()).thenReturn(oracleConfig.getUsername());
+//		when(service.getPassword()).thenReturn(oracleConfig.getPassword());
+//		when(termConfig.getOverride()).thenReturn("true");
+//		when(termConfig.getSemester()).thenReturn("202230");
+//		
+//	    BannerStudentInfo stu = new BannerStudentInfo();
+//		stu.setId("20221121");
+//		stu.setFirstname("Robin");
+//		stu.setLastname("Harris");
+//		stu.setFaculty("Applied Sciences");
+//		stu.setLevel("UNDERGRAD");
+//		list.add(stu);
+//		BannerStudentInfo stu1 = new BannerStudentInfo();
+//		stu1.setId("432321112");
+//		stu1.setFirstname("Anthony");
+//		stu1.setLastname("Mcdowell");
+//		stu1.setFaculty("Economics");
+//		stu1.setLevel("UNDERGRAD");
+//		list.add(stu1);
+//	    
+//	    
+//	    IDWorksInfo info = new IDWorksInfo();
+//	    info.setHolderid("20221121");
+//	    info.setFirstname("Robin");
+//		info.setLastname("Johnson");
+//		info.setFaculty("Applied Sciences");
+//		info.setLevel("UNDERGRAD");
+//		worksList.add(info);
+//		
+//		IDWorksInfo info1 = new IDWorksInfo();
+//		info1.setHolderid("432321112");
+//	    info1.setFirstname("Anthony");
+//		info1.setLastname("Mcdowell");
+//		info1.setFaculty("Economics");
+//		info1.setLevel("UNDERGRAD");
+//		worksList.add(info1);
+//		
+//		worksService.performUpdates();;
+//		
+//		verify(worksDao, times(1)).updateIDWorks(info);
+//		verify(worksDao, never()).insertToIDWorks(stu);
+//		verify(worksDao,  never()).updateIDWorks(info1);
+//		verify(worksDao, never()).insertToIDWorks(stu1);
 		
 		
 	}
 	
 	@Test
 	public void performanceUpdateVerifyOneInsertTest() throws Exception {
-		String selectStatement = "select * from spriden";
-	    ArrayList<BannerStudentInfo> studentList = new ArrayList<BannerStudentInfo>();
-	    ArrayList<IDWorksInfo> worksList = new ArrayList<IDWorksInfo>();
-	    oracleConnection = mock(OracleConnection.class);
-	    conn = mock(Connection.class);
-	    query = mock(Queries.class);
-	    service = mock(OracleConfig.class);
-	    
-	    BannerStudentInfo stu = new BannerStudentInfo();
-		stu.setId("20221121");
-		stu.setFirstname("Robin");
-		stu.setLastname("Johnson");
-		stu.setFaculty("Applied Sciences");
-		stu.setLevel("Undergraduate");
-		studentList.add(stu);
-		
-		BannerStudentInfo stu1 = new BannerStudentInfo();
-		stu1.setId("40003421");
-		stu1.setFirstname("Pamela");
-		stu1.setLastname("Jones");
-		stu1.setFaculty("Economics");
-		stu1.setLevel("Undergraduate");
-		studentList.add(stu1);
-	    
-		IDWorksInfo info = new IDWorksInfo();
-	    info.setHolderid("20221121");
-	    info.setFirstname("Robin");
-		info.setLastname("Johnson");
-		info.setFaculty("Applied Sciences");
-		info.setLevel("UNDERGRAD");
-		worksList.add(info);
-		
-		IDWorksInfo info1 = new IDWorksInfo();
-		info1.setHolderid("432321112");
-	    info1.setFirstname("Anthony");
-		info1.setLastname("Mcdowell");
-		info1.setFaculty("Economics");
-		info1.setLevel("UNDERGRAD");
-		worksList.add(info1);
-		
-		
-	    when(query.getStudentQuery()).thenReturn(selectStatement);
-	    when(oracleConnection.getConnection()).thenReturn(conn);
-	    when(conn.prepareStatement(query.getStudentQuery())).thenReturn(prepStmt);
-	    when(oracleDao.collectInfo(null)).thenReturn(studentList); 
-	    when(worksDao.gatherIDWorksData()).thenReturn(worksList);
-		when(service.getDriver()).thenReturn(oracleConfig.getDriver());
-		when(service.getConnect()).thenReturn(oracleConfig.getConnect());
-		when(service.getUsername()).thenReturn(oracleConfig.getUsername());
-		when(service.getPassword()).thenReturn(oracleConfig.getPassword());
-		
-		worksService.performUpdates();
-		
-		//verify(worksDao, times(1)).updateIDWorks(info);
-		verify(worksDao, never()).insertToIDWorks(stu);
-		//verify(worksDao,  times(1)).updateIDWorks(info1);
-		verify(worksDao, times(1)).insertToIDWorks(stu1);
-		
+//		String selectStatement = "select * from spriden";
+//	    ArrayList<BannerStudentInfo> studentList = new ArrayList<BannerStudentInfo>();
+//	    ArrayList<IDWorksInfo> worksList = new ArrayList<IDWorksInfo>();
+//	    oracleConnection = mock(OracleConnection.class);
+//	    conn = mock(Connection.class);
+//	    query = mock(Queries.class);
+//	    service = mock(OracleConfig.class);
+//	    
+//	    BannerStudentInfo stu = new BannerStudentInfo();
+//		stu.setId("20221121");
+//		stu.setFirstname("Robin");
+//		stu.setLastname("Johnson");
+//		stu.setFaculty("Applied Sciences");
+//		stu.setLevel("Undergraduate");
+//		studentList.add(stu);
+//		
+//		BannerStudentInfo stu1 = new BannerStudentInfo();
+//		stu1.setId("40003421");
+//		stu1.setFirstname("Pamela");
+//		stu1.setLastname("Jones");
+//		stu1.setFaculty("Economics");
+//		stu1.setLevel("Undergraduate");
+//		studentList.add(stu1);
+//	    
+//		IDWorksInfo info = new IDWorksInfo();
+//	    info.setHolderid("20221121");
+//	    info.setFirstname("Robin");
+//		info.setLastname("Johnson");
+//		info.setFaculty("Applied Sciences");
+//		info.setLevel("UNDERGRAD");
+//		worksList.add(info);
+//		
+//		IDWorksInfo info1 = new IDWorksInfo();
+//		info1.setHolderid("432321112");
+//	    info1.setFirstname("Anthony");
+//		info1.setLastname("Mcdowell");
+//		info1.setFaculty("Economics");
+//		info1.setLevel("UNDERGRAD");
+//		worksList.add(info1);
+//		
+//		
+//	    when(query.getStudentQuery()).thenReturn(selectStatement);
+//	    when(oracleConnection.getConnection()).thenReturn(conn);
+//	    when(conn.prepareStatement(query.getStudentQuery())).thenReturn(prepStmt);
+//	    when(oracleDao.collectInfo(null)).thenReturn(studentList); 
+//	    when(worksDao.gatherIDWorksData()).thenReturn(worksList);
+//		when(service.getDriver()).thenReturn(oracleConfig.getDriver());
+//		when(service.getConnect()).thenReturn(oracleConfig.getConnect());
+//		when(service.getUsername()).thenReturn(oracleConfig.getUsername());
+//		when(service.getPassword()).thenReturn(oracleConfig.getPassword());
+//		
+//		worksService.performUpdates();
+//		
+//		//verify(worksDao, times(1)).updateIDWorks(info);
+//		verify(worksDao, never()).insertToIDWorks(stu);
+//		//verify(worksDao,  times(1)).updateIDWorks(info1);
+//		verify(worksDao, times(1)).insertToIDWorks(stu1);
+//		
 		
 	}
 

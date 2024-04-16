e5ec49040967d8bc316ad1c052672def4a3c188f
Sheldon <sheldon.spencer@gmail.com>
2024-04-16 17:07:18 -0400

    update capitalization

--- src/main/java/com/uwi/idworks/service/IDWorksServiceImpl.java
+++ src/main/java/com/uwi/idworks/service/IDWorksServiceImpl.java
@@ -88,6 +88,10 @@ public class IDWorksServiceImpl implements IDWorksService  {
 							if (!m.getInitial().trim().equals(student.getInitial().trim())) {
 							  m.setInitial(student.getInitial());
 							}
+						} else {
+							if (student.getInitial() != null) {
+								m.setInitial(student.getInitial());
+							}
 						}
 						if (!m.getFirstname().trim().equals(student.getFirstname().trim())) {
 							m.setFirstname(student.getFirstname());

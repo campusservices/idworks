731bc87142a1c4eacafc8303c017288d5b9dd24a
Sheldon <sheldon.spencer@gmail.com>
2024-04-16 22:32:17 -0400

    update capitalization

--- src/main/java/com/uwi/idworks/service/IDWorksServiceImpl.java
+++ src/main/java/com/uwi/idworks/service/IDWorksServiceImpl.java
@@ -51,7 +51,7 @@ public class IDWorksServiceImpl implements IDWorksService  {
 		String overrideSemester = System.getenv("ENV_OVERRIDE_SEMESTER") != null ? System.getenv("ENV_OVERRIDE_SEMESTER"):"false";
 		String term = overrideSemester == "true" && System.getenv("ENV_SEMESTER") != null ? System.getenv("ENV_SEMESTER") :null;
 		logger.info("term - {} {}", System.getenv("ENV_SEMESTER"), System.getenv("ENV_OVERRIDE_SEMESTER"));
-		//term = "202410";
+//		term = "202410";
 		try {
 			ArrayList<BannerStudentInfo>  studentList = oracleDao.collectInfo(term);
 			ArrayList<IDWorksInfo> worksList = worksDao.gatherIDWorksData();

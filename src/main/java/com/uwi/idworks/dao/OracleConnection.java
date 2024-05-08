a30ca72e70ca76964d3de95c63da4ff6e743771c
Sheldon <sheldon.spencer@gmail.com>
2024-05-08 10:15:08 -0400

    oracle timeout correction

--- src/main/java/com/uwi/idworks/dao/OracleConnection.java
+++ src/main/java/com/uwi/idworks/dao/OracleConnection.java
@@ -3,6 +3,7 @@ package com.uwi.idworks.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,9 +29,12 @@ public class OracleConnection {
 	public void connectDataSource() {
 		
 		try {
-
+			System.setProperty("oracle.jdbc.javaNetNio", "false");
 			logger.info("Starting Oracle Db");
 			Class.forName(service.getDriver());
+			Properties prop = new Properties();
+			prop.put("oracle.jdbc.javaNetNio", false);
+			
 			conn = DriverManager.getConnection(service.getConnect(), 
 															service.getUsername(), 
 															service.getPassword());
